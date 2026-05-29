a665fd6ce8fb43a30220c43686a5c4a065c5c708
HauLTCE <letrunghau2244@gmail.com>
2026-05-29 22:06:04 +0700

    feat: backend nuke -> microservice

--- Attrition_Web/api/Repositories/IForumRepository.cs
+++
@@ -1,9 +0,0 @@
-using Attrition.API.DTOs;
-using Attrition.API.Models;
-
-namespace Attrition.API.Repositories;
-
-public interface IForumRepository : IRepository<ForumThread>
-{
-    Task<PaginatedResponse<UserPostDto>> GetUserPostsPagedAsync(Guid userId, int page, int pageSize);
-}
--- Attrition_Web/api/Repositories/IWikiRepository.cs
+++
@@ -1,9 +0,0 @@
-using Attrition.API.DTOs;
-using Attrition.API.Models;
-
-namespace Attrition.API.Repositories;
-
-public interface IWikiRepository : IRepository<WikiArticle>
-{
-    Task<PaginatedResponse<UserContributionDto>> GetUserContributionsPagedAsync(Guid userId, int page, int pageSize);
-}
