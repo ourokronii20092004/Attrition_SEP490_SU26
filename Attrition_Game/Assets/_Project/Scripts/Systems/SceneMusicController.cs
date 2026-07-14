deeaf7f48e83bb3204b2eb779f35f631a8fe672d
Dangtran1301 <dangdeptrai1212@gmail.com>
2026-07-14 12:49:20 +0700

    Add background music in scene

---
+++ Attrition_Game/Assets/_Project/Scripts/Systems/SceneMusicController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Attrition.Systems
+{
+    public class SceneMusicController : MonoBehaviour
+    {
+        [Header("Scene Music Configuration")]
+        [SerializeField] private AudioClip sceneBgmClip;
+        [SerializeField][Range(0f, 1f)] private float volume = 0.7f;
+        [SerializeField] private float fadeDuration = 0.8f;
+
+        void Start()
+        {
+            if (sceneBgmClip != null)
+            {
+                GameBgm.Instance.Play(sceneBgmClip, volume, fadeDuration);
+        }
+        }
+    }
+}
